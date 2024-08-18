8b97964a76a90f8cdba6256251f65a45b9436701
AveGus(Eldar) <garipov.eldar200@gmail.com>
2024-08-18 21:33:42 +0500

    Auth module and another things

---
+++ backendSPECreator/SPE-backend/src/projects/models.py
@@ -0,0 +1,10 @@
+# from sqlalchemy import Table, Column, Integer, ForeignKey
+# from services.database import metadata
+#
+#
+# project = Table(
+#     "project",
+#     metadata,
+#     Column("id",  ForeignKey(user.c.id),
+#
+# )
